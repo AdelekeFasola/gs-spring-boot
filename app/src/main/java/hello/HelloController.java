879beaf8ecd8790b3e8b978445bba34a0831029b
adeleke fasola <a.fasola@omgroupinc.us>
2019-07-03 16:10:20 -0400

    now resolved

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot still not working!";
+        return "Hello from Spring Boot!";
     }   
 }
